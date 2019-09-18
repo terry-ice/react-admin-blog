ea652be90bf560cec1c6ed343ab8fe4c49f830d8
terry-ice <326959013@qq.com>
2019-09-18 23:17:55 +0800

    label add

---
+++ src/pages/Tags/List/index.tsx
@@ -0,0 +1,67 @@
+import LabelContainer from '@/graphql/label';
+import { Divider, Table } from 'antd';
+import React from 'react';
+import ArticleList from './style';
+const columns = [
+  {
+    title: '名称',
+    dataIndex: 'name'
+  },
+  {
+    title: '描述',
+    dataIndex: 'description'
+  },
+  {
+    title: '别名',
+    dataIndex: 'slug'
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text: any, record: any) => (
+      <span>
+        <a href="javascript:;">编辑</a>
+        <Divider type="vertical" />
+        <a href="javascript:;">查看</a>
+      </span>
+    )
+  }
+];
+
+class TagList extends React.Component {
+  state = {
+    selectedRowKeys: [] // Check here to configure the default column
+  };
+
+  onSelectChange = (selectedRowKeys: string[]) => {
+    this.setState({ selectedRowKeys });
+  };
+
+  render() {
+    const { selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+      hideDefaultSelections: true
+    };
+    return (
+      <ArticleList>
+        <LabelContainer>
+          {({ getLabel: { loading, data } }: any) => {
+            const dataList = data && data.labels ? data.labels : [];
+            return (
+              <Table
+                loading={loading}
+                className="articleList"
+                rowSelection={rowSelection}
+                columns={columns}
+                dataSource={dataList}
+              />
+            );
+          }}
+        </LabelContainer>
+      </ArticleList>
+    );
+  }
+}
+export default TagList;
--- src/pages/Tags/index.tsx
+++ src/pages/Tags/index.tsx
@@ -1,46 +1,90 @@
-import { StyleBtn, StyleLabel } from "@/assets/style/common";
-import Tag from "@/components/Tag";
-import { Input } from "antd";
-import React from "react";
+import { StyleBtn, StyleLabel } from '@/assets/style/common';
+import useFrom from '@/components/UseFrom';
+import LabelContainer from '@/graphql/label';
+import { Input, message } from 'antd';
+import React from 'react';
+import TagList from './List';
 const { TextArea } = Input;
-import Style from "./style";
+import Style from './style';
 
 export default () => {
+  const [values, handelChange] = useFrom({
+    name: '',
+    slug: '',
+    description: ''
+  });
   return (
     <Style>
       <div className="post-body-content">
-        <Tag />
+        <TagList />
       </div>
       <div className="postbox-container">
         <div className="postbox-container-submit">
           <div className="postbox-container-title">发布</div>
-          <StyleLabel width="200px">
-            <label htmlFor="title">
-              名称：
-              <input type="text" name="title" placeholder="title" />
-            </label>
-          </StyleLabel>
-          <StyleLabel width="200px">
-            <label htmlFor="title">
-              别名：
-              <input type="text" name="title" placeholder="title" />
-            </label>
-          </StyleLabel>
-          <StyleLabel width="200px">
-            <label htmlFor="title">
-              icon:
-              <input type="text" name="title" placeholder="title" />
-            </label>
-          </StyleLabel>
-          <StyleLabel width="200px">
-            <label htmlFor="title">
-              描述:
-              <TextArea placeholder="title" />
-            </label>
-          </StyleLabel>
-          <StyleBtn width="200px" height="35px">
-            <button type="submit">发布</button>
-          </StyleBtn>
+          <LabelContainer>
+            {({ addLabel }: any) => {
+              return (
+                <form
+                  method="post"
+                  onSubmit={async (e: React.FormEvent<HTMLFormElement>) => {
+                    e.preventDefault();
+                    addLabel
+                      .mutation({
+                        variables: { ...values }
+                      })
+                      .then((res: any) => {
+                        message.success('添加成功');
+                      });
+                  }}
+                >
+                  <StyleLabel width="200px">
+                    <label htmlFor="title">
+                      名称：
+                      <input
+                        type="text"
+                        name="name"
+                        value={values.name}
+                        onChange={handelChange}
+                        placeholder="title"
+                      />
+                    </label>
+                  </StyleLabel>
+                  <StyleLabel width="200px">
+                    <label htmlFor="title">
+                      别名：
+                      <input
+                        type="text"
+                        name="slug"
+                        value={values.slug}
+                        onChange={handelChange}
+                        placeholder="title"
+                      />
+                    </label>
+                  </StyleLabel>
+                  <StyleLabel width="200px">
+                    <label htmlFor="title">
+                      icon:
+                      <input type="text" name="title" placeholder="title" />
+                    </label>
+                  </StyleLabel>
+                  <StyleLabel width="200px">
+                    <label htmlFor="title">
+                      描述:
+                      <TextArea
+                        value={values.description}
+                        name="description"
+                        onChange={handelChange}
+                        placeholder="title"
+                      />
+                    </label>
+                  </StyleLabel>
+                  <StyleBtn width="200px" height="35px">
+                    <button type="submit">发布</button>
+                  </StyleBtn>
+                </form>
+              );
+            }}
+          </LabelContainer>
         </div>
       </div>
     </Style>
